de1d9c5c5473f100c9f6ee5c66e74bf6f60ed92c
sonawanesrushti261-rgb <sonawanesrushti261@gmail.com>
2026-08-04 19:19:55 +0530

    Time: 3 ms (67.13%), Space: 47.4 MB (88.6%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findErrorNums(int[] arr) {
+        int i = 0;
+        while(i <arr.length){
+            int correctIdx = arr[i] - 1;
+           if(arr[i] != arr[correctIdx]){
+                swap(arr, i, correctIdx);
+            }else{
+                i++;
+            }
+        }
+
+        //search for missing num
+        for(int index=0; index<arr.length; index++){
+            if(arr[index] != index+1){
+                return new int[]{arr[index], index+1};  //repeating, missing
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
