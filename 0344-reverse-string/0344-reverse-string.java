91f31658c7277f2aeaa097efb1728d0f4551b558
sonawanesrushti261-rgb <sonawanesrushti261@gmail.com>
2026-08-13 19:28:14 +0530

    Time: 1 ms (23.08%), Space: 48.8 MB (6.41%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,15 +1,32 @@
 class Solution {
-    public void reverseString(char[] s) {
-        int start = 0;
-        int end = s.length-1;
-        while(start < end){
-            //swap
-            char temp = s[start];
-            s[start] = s[end];
-            s[end] = temp;
-
-            start++;
-            end--;
+    public void reverseStringHelper(char[] s, int start, int end){
+        //base condition
+        if(start >= end){
+            return;
         }
+
+        //swap
+        char temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        reverseStringHelper(s, start + 1, end - 1);
+    }
+    public void reverseString(char[] s) {
+        // this was a normal way just like how we reverse an array similar
+        // int start = 0;
+        // int end = s.length-1;
+        // while(start < end){
+        //     //swap
+        //     char temp = s[start];
+        //     s[start] = s[end];
+        //     s[end] = temp;
+
+        //     start++;
+        //     end--;
+        // }
+
+
+        //call that recursive fun
+        reverseStringHelper(s, 0, s.length-1);
     }
 }
