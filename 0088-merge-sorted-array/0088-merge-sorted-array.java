6b295e000abf0eb395343ff6ee2991a4aed63489
sonawanesrushti261-rgb <sonawanesrushti261@gmail.com>
2026-08-17 20:58:40 +0530

    Time: 0 ms (100%), Space: 43.7 MB (63.7%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,9 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //approach 1 - brute force sol
-        for(int i = 0; i < n; i++){
-            nums1[i + m] = nums2[i];
+        int p1 = m - 1;
+        int p2 = n - 1;
+
+        for(int p = m+n-1; p >= 0; p--){
+            if(p2<0){
+                break;
+            }
+            if( p1 >= 0&& nums1[p1] > nums2[p2]){
+                nums1[p] = nums1[p1];
+                p1--;
+            }else{
+                nums1[p] = nums2[p2];
+                p2--;
+            }
         }
-        Arrays.sort(nums1);
     }
 }
