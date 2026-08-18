7d8549a424e06769fcfcf101faf390822cad7f54
sonawanesrushti261-rgb <sonawanesrushti261@gmail.com>
2026-08-18 12:22:33 +0530

    Time: 0 ms (100%), Space: 43.1 MB (97.73%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,30 @@
+class Solution {
+    private void swap(int[] arr, int p1, int p2){
+        int temp = arr[p1];
+        arr[p1] = arr[p2];
+        arr[p2] = temp;
+    }
+    public void sortColors(int[] nums) {
+        int start = 0;
+        int mid = 0;
+        int end = nums.length - 1;
+
+        while(mid <= end){
+
+        switch(nums[mid]){
+            case 0:
+                swap(nums , start, mid);
+                mid++;
+                start++;
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+            swap(nums, mid, end);
+            end--;
+            break;
+        }
+    }
+}
+}
